2bf63a8c2933acc62b272af69a4d5e606cc067d7
Jackie <jgccasale@gmail.com>
2025-09-12 16:54:38 -0400

    Feat: wrote test cases for empty fields, whitespace, empty lines, double quoted fields, and fields with commas in the data using the data I added to the CSV files. Discovered an error with how the CSV parser handles commas and empty lines.

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -22,3 +22,32 @@ test("parseCSV yields only arrays", async () => {
   }
 });
 
+test("parseCSV splits correctly when there are commas in the data", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH)
+  expect(results[6]).toEqual(["Smith, John", "30"]);
+})
+
+test("parseCSV yields empty fields", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH)
+  expect(results[7]).toEqual(["\"\"", "10"]);
+  expect(results[8]).toEqual(["Josh", ""]);
+})
+
+test("parseCSV ignores empty lines", async () => {
+  const testResults = await parseCSV(TESTDATA_CSV_PATH)
+  expect(testResults).toHaveLength(2);
+  expect(testResults[0]).toEqual(["a", "b"]);
+  expect(testResults[1]).toEqual(["c"]);
+})
+
+test("parseCSV handles double-quoted fields", async () => {
+  const testResults = await parseCSV(TESTDATA_CSV_PATH)
+  expect(testResults[3]).toEqual(["\"he said \"hello world\"\"", "John"]);
+})
+
+test("parseCSV ignores spaces around values", async () => {
+  const testResults = await parseCSV(TESTDATA_CSV_PATH)
+  expect(testResults[4]).toEqual(["hi", "there"]);
+  expect(testResults[5]).toEqual(["this is one field", "this is another"]);
+})
+
